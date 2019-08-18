c72fc697a486245f9c85fbd235d031b153ffd2a7
suphero <harunsokullu@gmail.com>
2019-08-19 00:26:46 +0300

    change icon

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -48,9 +48,9 @@ const siteConfig = {
   users,
 
   /* path to images for header/footer */
-  headerIcon: 'img/favicon.ico',
-  footerIcon: 'img/favicon.ico',
-  favicon: 'img/favicon.ico',
+  headerIcon: 'img/HS.png',
+  footerIcon: 'img/HS.png',
+  favicon: 'img/HS.png',
 
   /* Colors for website */
   colors: {
